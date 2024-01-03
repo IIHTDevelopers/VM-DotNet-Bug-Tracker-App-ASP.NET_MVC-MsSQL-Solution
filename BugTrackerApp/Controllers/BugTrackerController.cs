f15eddb377736cfdf71d60a08e37cb5b110c48c0
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-03 15:13:54 +0530

    Add files via upload

---
+++ BugTrackerApp/Controllers/BugTrackerController.cs
@@ -0,0 +1,116 @@
+﻿using BugTrackerApp.DAL.Interface;
+using BugTrackerApp.DAL.Repository;
+using BugTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BugTrackerApp.Controllers
+{
+    public class BugTrackerController : Controller
+    {
+        private readonly IBugTrackerInterface _Repository;
+        public BugTrackerController(IBugTrackerInterface service)
+        {
+            _Repository = service;
+        }
+        public BugTrackerController()
+        {
+            // Constructor logic, if needed
+        }
+        // GET: BugTracker
+        public ActionResult Index()
+        {
+            var Bugs = from work in _Repository.GetBugs()
+                        select work;
+            return View(Bugs);
+        }
+
+        public ViewResult Details(int id)
+        {
+            Bug Bug =   _Repository.GetBugByID(id);
+            return View(Bug);
+        }
+
+        public ActionResult Create()
+        {
+            return View(new Bug());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Bug Bug)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _Repository.InsertBug(Bug);
+                    _Repository.Save();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DataException)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            return View(Bug);
+        }
+
+        public ActionResult EditAsync(int id)
+        {
+            Bug Bug =  _Repository.GetBugByID(id);
+            return View(Bug);
+        }
+        [HttpPost]
+        public ActionResult Edit(Bug Bug)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _Repository.UpdateBug(Bug);
+                    _Repository.Save();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DataException)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            return View(Bug);
+        }
+
+        public ActionResult Delete(int id, bool? saveChangesError)
+        {
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Unable to save changes. Try again, and if the problem persists see your system administrator.";
+            }
+            Bug Bug =  _Repository.GetBugByID(id);
+            return View(Bug);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                Bug Bug =  _Repository.GetBugByID(id);
+                _Repository.DeleteBug(id);
+                _Repository.Save();
+            }
+            catch (DataException)
+            {
+                return RedirectToAction("Delete",
+                   new System.Web.Routing.RouteValueDictionary {
+        { "id", id },
+        { "saveChangesError", true } });
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BugTrackerApp/DAL/Repository/BugTrackerRepository.cs
@@ -0,0 +1,64 @@
+﻿using BugTrackerApp.DAL.Interface;
+using BugTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BugTrackerApp.DAL.Repository
+{
+    public class BugTrackerRepository : IBugTrackerRepository
+    {
+        private BugTrackerDbContext _context;
+        public BugTrackerRepository(BugTrackerDbContext Context)
+        {
+            this._context = Context;
+        }
+        public IEnumerable<Bug> GetBugs()
+        {
+             return _context.Bugs.ToList();
+        }
+        public Bug GetBugByID(int id)
+        {
+            return _context.Bugs.Find(id);
+        }
+        public Bug InsertBug(Bug Bug)
+        {
+            return _context.Bugs.Add(Bug);
+        }
+        public int DeleteBug(int BugID)
+        {
+            Bug Bug = _context.Bugs.Find(BugID);
+            var res= _context.Bugs.Remove(Bug);
+            return res.Id;
+        }
+        public bool UpdateBug(Bug Bug)
+        {
+            var res= _context.Entry(Bug).State = EntityState.Modified;
+            return res.Equals("Bug");
+        }
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
